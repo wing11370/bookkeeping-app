42f45522f2e5e660e188e28e806750bf89a63a0b
Chadwick <wing11370@gmail.com>
2023-05-17 18:06:26 +0800

    docs: 定義Info、Server

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -2,6 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="developer@example.com"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Localhost"
+ * )
+ */
 class BaseController extends Controller
 {
 
