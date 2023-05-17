1ddab33a47e1a30772290d1da0b4d886541c9326
Chadwick <wing11370@gmail.com>
2023-05-17 17:58:14 +0800

    docs: swagger文件，使用者註冊、登入

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,14 +6,47 @@ use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
 
-class UserController extends Controller
+class UserController extends BaseController
 {
+
     public function __construct(protected User $user, protected UserService $userService)
     {
     }
-
     /**
+     * @OA\POST(
+     *      path="/api/register",
+     *      operationId="register",
+     *      tags={"使用者"},
+     *      summary="註冊會員",
+     *      description="註冊會員",
+     *     @OA\RequestBody(
+     *     description="註冊會員",
+     *     required=true,
+     *          @OA\JsonContent(
+     *              required={"name","email","password"},
+     *              @OA\Property(property="name", type="string", example="test"),
+     *              @OA\Property(property="email", type="string", example="test@test.com"),
+     *              @OA\Property(property="password", type="string", example="test1234"),
+     *          ),
+     *     ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="註冊成功",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="token", type="string", example="token"),
+     *          )
+     *       ),
+     *     @OA\Response(
+     *          response=400,
+     *          description="註冊失敗",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="xxxx"),
+     *          )
+     *      ),
+     * )
+     *
      * @param Request $request
      * @return JsonResponse
      */
@@ -40,6 +73,38 @@ class UserController extends Controller
     }
 
     /**
+     * @OA\POST(
+     *     path="/api/login",
+     *     operationId="login",
+     *     tags={"使用者"},
+     *     summary="登入",
+     *     description="登入",
+     *     @OA\RequestBody(
+     *          description="登入",
+     *          required=true,
+     *              @OA\JsonContent(
+     *                  required={"email","password"},
+     *                  @OA\Property(property="email", type="string", example="test@test.com"),
+     *                  @OA\Property(property="password", type="string", example="test1234"),
+     *              ),
+     *          ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="登入成功",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="token", type="string", example="token"),
+     *          )
+     *      ),
+     *     @OA\Response(
+     *          response=400,
+     *          description="登入失敗",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="xxxx"),
+     *          )
+     *     ),
+     * )
+     *
+     *
      * @param Request $request
      * @return JsonResponse
      */
